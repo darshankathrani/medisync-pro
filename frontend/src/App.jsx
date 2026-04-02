751077ca36a739799cca9c637504a30d1c4bba5e
darshan <darshankathrani6@gmail.com>
2026-04-03 00:48:53 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetch("https://medisync-pro.onrender.com")
+      .then(() => console.log("Backend awake"))
+      .catch(() => console.log("Wake-up failed"));
+  }, []);
+
   return (
     <>
       <Navbar />
